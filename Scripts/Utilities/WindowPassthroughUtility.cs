6d1169a2e430288e6b6dead502648eebe07a2deb
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-04-14 21:09:31 +0100

    Use `LibraryImport`

--- Scripts/Utilities/WindowPassthroughUtility.cs
+++ Scripts/Utilities/WindowPassthroughUtility.cs
@@ -2,21 +2,19 @@ using Godot;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
-#pragma warning disable SYSLIB1054
-
 // Credit to KitzuGG:
 // https://forum.godotengine.org/t/mousepassthrough-for-non-same-applications/49284/3
 
 [SupportedOSPlatform("Windows")]
-public static class WindowPassthroughUtility {
+public static partial class WindowPassthroughUtility {
 	private const int GWL_EXSTYLE = -20;
 	private const long WS_EX_TRANSPARENT = 0x00000020L;
 	private const long WS_EX_LAYERED = 0x00080000L;
 
-	[DllImport("user32.dll")]
-	private static extern long GetWindowLong(int hWnd, int nIndex);
-	[DllImport("user32.dll")]
-	private static extern int SetWindowLong(int hWnd, int nIndex, long dwNewLong);
+	[LibraryImport("user32.dll")]
+	private static partial long GetWindowLong(int hWnd, int nIndex);
+	[LibraryImport("user32.dll")]
+	private static partial int SetWindowLong(int hWnd, int nIndex, long dwNewLong);
 
 	public static void SetWindowPassthrough(int HWND, bool Enable) {
 		long CurrentWindowStyle = GetWindowLong(HWND, GWL_EXSTYLE);
